61cdfa0b451b42fd4618fa7e359eec387f594630
Nicollas Matos <168081344+Nic62@users.noreply.github.com>
2025-03-26 22:39:25 -0300

    aba quase pronta

--- aba.py
+++ aba.py
@@ -1,26 +1,24 @@
 import streamlit as st
 import pandas as pd
 import re
-
-# Configuração da página com a imagem como ícone
+# import joblib as jb
+#.\venv\Scripts\activate
+# streamlit run aba.py
 st.set_page_config(
     page_title="Aba de Prioridades", 
     page_icon="https://logospng.org/download/grupo-caoa/logo-caoa-2048.png"
 )
 
-# Criando layout com título e imagem lado a lado
-col1, col2 = st.columns([0.85, 0.15])  # Ajuste de proporções para melhor alinhamento
+col1, col2 = st.columns([0.85, 0.15])
 
 with col1:
-    st.title("Prioridades")  # Título bem posicionado
+    st.title("Prioridades")
 
 with col2:
     st.image("https://logospng.org/download/grupo-caoa/logo-caoa-2048.png", width=120)  # Imagem ao lado
 
 st.divider()
 st.header('Digite as informações abaixo:')
-
-# Entrada de dados
 info_identificacao = st.text_input("Digite identificação:")
 if info_identificacao and not re.match("^[A-Za-z.]+$", info_identificacao):
     st.error("Erro: Apenas letras (sem acentos) e ponto são permitidos!")
@@ -28,11 +26,11 @@ if info_identificacao and not re.match("^[A-Za-z.]+$", info_identificacao):
 id_peca = st.text_input("Digite a peça:")
 info_prio = st.radio("Selecione o grau de prioridade:", ['Por volta de 40 unidades', 'Menos de 20 unidades', 'Sem saldo no módulo'])
 
-# Lista para armazenar dados temporários
+# armazenamento
 if "dados" not in st.session_state:
     st.session_state["dados"] = []
 
-# Botão para adicionar os dados
+# Botão 1
 if st.button("Priorizar"):
     if info_identificacao and id_peca:
         novo_dado = {"Identificação": info_identificacao, "Peça": id_peca, "Prioridade": info_prio}
@@ -41,7 +39,7 @@ if st.button("Priorizar"):
     else:
         st.warning("Preencha todos os campos antes de priorizar!")
 
-# Exibir tabela com os dados
+# Tabela
 if st.session_state["dados"]:
     st.subheader("Prioridades Concluídas")
     df = pd.DataFrame(st.session_state["dados"])
