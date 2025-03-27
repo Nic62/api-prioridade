70bbb026986697cceecc6cd3301a7781ba156af6
Nicollas Matos <168081344+Nic62@users.noreply.github.com>
2025-03-27 05:19:56 -0300

    Atualizar o aba.py

--- aba.py
+++ aba.py
@@ -7,17 +7,22 @@ import webbrowser
 #.\venv\Scripts\activate
 # streamlit run aba.py
 
+import streamlit as st
+import pandas as pd
+import re
+import webbrowser
+
 st.set_page_config(
     page_title="Aba de Prioridades", 
     page_icon="https://logospng.org/download/grupo-caoa/logo-caoa-2048.png"
 )
 
-# 1. Imagem no topo
 st.image("https://logospng.org/download/grupo-caoa/logo-caoa-2048.png", width=150)  
 st.title("Prioridades")
 
 st.divider()
 st.header('Digite as informações abaixo:')
+
 info_identificacao = st.text_input("Digite identificação:")
 if info_identificacao and not re.match("^[A-Za-z.]+$", info_identificacao):
     st.error("Erro: Apenas letras (sem acentos) e ponto são permitidos!")
@@ -25,25 +30,26 @@ if info_identificacao and not re.match("^[A-Za-z.]+$", info_identificacao):
 id_peca = st.text_input("Digite a peça:")
 info_prio = st.radio("Selecione o grau de prioridade:", ['Por volta de 40 unidades', 'Menos de 20 unidades', 'Sem saldo no módulo'])
 
-# armazenamento
 if "dados" not in st.session_state:
     st.session_state["dados"] = []
 
-# Botão 1
 if st.button("Priorizar"):
-     if info_identificacao and id_peca:
-         novo_dado = {"Identificação": info_identificacao, "Peça": id_peca, "Prioridade": info_prio}
-         st.session_state["dados"].append(novo_dado)
-         st.success("Peça priorizada com sucesso!")
-        else:
-         st.warning("Preencha todos os campos antes de priorizar!")
+    if info_identificacao and id_peca:
+        novo_dado = {"Identificação": info_identificacao, "Peça": id_peca, "Prioridade": info_prio}
+        st.session_state["dados"].append(novo_dado)
+        st.session_state["mostrar_botao"] = True
+        st.success("Peça priorizada com sucesso!")
+    else:
+        st.warning("Preencha todos os campos antes de priorizar!")
 
-# Tabela
 if st.session_state["dados"]:
     st.subheader("Prioridades Concluídas")
     df = pd.DataFrame(st.session_state["dados"])
     st.dataframe(df)
-    # botao2
-if st.button("Acessar Planilha Google",False): webbrowser.open("https://docs.google.com/spreadsheets/d/1jbI9wN9ny8HCJPOX66i69zdCSp6eoHNK9V5IhJ5ftMk/edit?gid=0#gid=0")
+
+if st.session_state.get("mostrar_botao", False):  
+    if st.button("Acessar Planilha Google"):
+        webbrowser.open("https://docs.google.com/spreadsheets/d/1jbI9wN9ny8HCJPOX66i69zdCSp6eoHNK9V5IhJ5ftMk/edit?gid=0#gid=0")
+
 if st.button("Acessar Looker"):
     webbrowser.open("https://looker.com")
