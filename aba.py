cd5f1f1e23747f5e00f09028d6a5437eb480e912
Nicollas Matos <168081344+Nic62@users.noreply.github.com>
2025-03-27 21:37:10 -0300

    Update aba.py

--- aba.py
+++ aba.py
@@ -4,15 +4,14 @@ import re
 from datetime import datetime
 
 # import joblib as jb
-#.\venv\Scripts\activate
+# .\venv\Scripts\activate
 # streamlit run aba.py
 
 st.set_page_config(
     page_title="Aba de Prioridades", 
-    page_icon="https://logospng.org/download/grupo-caoa/logo-caoa-2048.png"
-)
+    page_icon="https://logospng.org/download/grupo-caoa/logo-caoa-2048.png")
 
-st.image("https://logospng.org/download/grupo-caoa/logo-caoa-2048.png", width=150)  
+st.image("https://logospng.org/download/grupo-caoa/logo-caoa-2048.png", width=150)
 
 st.markdown(
     "<h1 style='text-align: center;'>Prioridades</h1>", 
@@ -28,9 +27,9 @@ if info_identificacao and not re.match("^[A-Za-z.]+$", info_identificacao):
 id_peca = st.text_input("Digite a peça:")
 info_prio = st.radio("Selecione o grau de prioridade:", ['Por volta de 40 unidades', 'Menos de 20 unidades', 'Sem saldo no módulo'])
 
+
 if "dados" not in st.session_state:
     st.session_state["dados"] = []
-
 if info_identificacao and id_peca:
     agora = datetime.now()
     novo_dado = {
@@ -38,15 +37,13 @@ if info_identificacao and id_peca:
         "Peça": id_peca,
         "Prioridade": info_prio,
         "Data": agora.strftime("%Y-%m-%d"),
-        "Hora": agora.strftime("%H:%M:%S")
-    }
+        "Hora": agora.strftime("%H:%M:%S")}
     
     st.session_state["dados"].append(novo_dado)
     st.session_state["mostrar_botao"] = True
     st.success("Peça priorizada com sucesso!")
 else:
     st.warning("Preencha todos os campos antes de priorizar!")
-
 if st.session_state["dados"]:
     st.subheader("Prioridades Concluídas")
     df = pd.DataFrame(st.session_state["dados"])
