b9f94e7d6f5e52c16d1850dbf2e6911a8eeb6ee5
Nicollas Matos <168081344+Nic62@users.noreply.github.com>
2025-03-27 21:17:40 -0300

    Update aba.py

--- aba.py
+++ aba.py
@@ -4,10 +4,22 @@ import re
 from datetime import datetime
 
 
-onedrive_link = "https://onedrive.live.com/download?cid=8388a7abc69fb78d&resid=8388A7ABC69FB78D%21123&authkey=ABC123"
+# import joblib as jb
+#.\venv\Scripts\activate
+# streamlit run aba.py
 
-st.set_page_config(page_title="Aba de Prioridades", page_icon="📋")
+st.set_page_config(
+    page_title="Aba de Prioridades", 
+    page_icon="https://logospng.org/download/grupo-caoa/logo-caoa-2048.png"
+)
 
+st.image("https://logospng.org/download/grupo-caoa/logo-caoa-2048.png", width=150)  
+
+st.markdown(
+    "<h1 style='text-align: center;'>Prioridades</h1>", 
+    unsafe_allow_html=True
+)
+st.divider()
 st.header('Digite as informações abaixo:')
 
 info_identificacao = st.text_input("Digite identificação:")
@@ -17,34 +29,15 @@ if info_identificacao and not re.match("^[A-Za-z.]+$", info_identificacao):
 id_peca = st.text_input("Digite a peça:")
 info_prio = st.radio("Selecione o grau de prioridade:", ['Por volta de 40 unidades', 'Menos de 20 unidades', 'Sem saldo no módulo'])
 
+if "dados" not in st.session_state:
+    st.session_state["dados"] = []
+
 if st.button("Priorizar"):
     if info_identificacao and id_peca:
-        novo_dado = {
-            "Identificação": info_identificacao,
-            "Peça": id_peca,
-            "Prioridade": info_prio,
-            "Data e Hora": datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-        }
-        
-        try:
-            df_existente = pd.read_excel(onedrive_link)
-            df_final = pd.concat([df_existente, pd.DataFrame([novo_dado])], ignore_index=True)
-        except:
-            df_final = pd.DataFrame([novo_dado])
-        
-        df_final.to_excel("prioridades.xlsx", index=False)
-
+        novo_dado = {"Identificação": info_identificacao, "Peça": id_peca, "Prioridade": info_prio, "Data": agora.strftime("%Y-%m-%d"),
+            "Hora": agora.strftime("%H:%M:%S")}
+        st.session_state["dados"].append(novo_dado)
+        st.session_state["mostrar_botao"] = True
         st.success("Peça priorizada com sucesso!")
     else:
         st.warning("Preencha todos os campos antes de priorizar!")
-
-st.link_button("Acessar Planilha no OneDrive", onedrive_link)
-
-
-if st.session_state["dados"]:
-    st.subheader("Prioridades Concluídas")
-    df = pd.DataFrame(st.session_state["dados"])
-    st.dataframe(df)
-
-st.link_button("Acessar Planilha", "")
-st.link_button("Acessar BI", "https://docs.google.com/spreadsheets/d/1jbI9wN9ny8HCJPOX66i69zdCSp6eoHNK9V5IhJ5ftMk/edit?gid=0#gid=0")
