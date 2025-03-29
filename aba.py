69a339f3804d59388ff52ec24d40d348d441d024
Nicollas Matos <168081344+Nic62@users.noreply.github.com>
2025-03-29 00:42:34 -0300

    Update aba.py

--- aba.py
+++ aba.py
@@ -17,7 +17,7 @@ st.markdown(
     "<h1 style='text-align: center;'>Prioridades</h1>", 
     unsafe_allow_html=True)
 st.divider()
-st.page_link("solicitacao.py", label="Acessar Relatório", icon="📄")
+st.page_link("solicitacao.py", label="solicitação", icon="📄")
 
 st.header('Digite as informações abaixo:')
 
