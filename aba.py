3085b75d0cc302e0455bbd97cfb2ab94cb2d8b7b
Nicollas Matos <168081344+Nic62@users.noreply.github.com>
2025-03-30 15:11:12 -0300

    Update aba.py

--- aba.py
+++ aba.py
@@ -4,22 +4,37 @@ import re
 from datetime import datetime
 import pytz
 
-# import joblib as jb
-# .\venv\Scripts\activate
-# streamlit run aba.py
+
 fuso_horario = pytz.timezone("America/Sao_Paulo")
 st.set_page_config(
     page_title="Aba de Prioridades", 
-    page_icon="https://logospng.org/download/grupo-caoa/logo-caoa-2048.png")
+    page_icon="https://logospng.org/download/grupo-caoa/logo-caoa-2048.png"")
+
+
+st.markdown(
+    """
+    <style>
+        .main-container {
+            border: 4px solid #004080;
+            border-radius: 10px;
+            box-shadow: 5px 5px 15px rgba(0, 0, 0, 0.3);
+            padding: 20px;
+            background-color: #f9f9f9;
+        }
+    </style>
+    <div class='main-container'>
+    """,
+    unsafe_allow_html=True
+)
+
 
 col1, col2, col3 = st.columns(3)
 with col2:
     st.image("https://logospng.org/download/grupo-caoa/logo-caoa-2048.png", width=200)
-st.markdown(
-    "<h1 style='text-align: center;'>Prioridades</h1>", 
-    unsafe_allow_html=True)
-st.divider()
 
+# Título
+st.markdown("<h1 style='text-align: center;'>Prioridades</h1>", unsafe_allow_html=True)
+st.divider()
 
 st.header('Digite as informações abaixo:')
 
@@ -35,21 +50,22 @@ if "dados" not in st.session_state:
     
 col1, col2, col3 = st.columns(3)
 with col2:
- if st.button("Priorizar"):
-   if info_identificacao and id_peca:
-        agora = datetime.now(fuso_horario)
-        novo_dado = {
-            "Identificação": info_identificacao,
-            "Peça": id_peca,
-            "Prioridade": info_prio,
-            "Data": agora.strftime("%Y-%m-%d"),
-            "Hora": agora.strftime("%H:%M:%S")}
-        
-        st.session_state["dados"].append(novo_dado)
-        st.session_state["mostrar_botao"] = True
-        st.success("Peça priorizada com sucesso!")
-   else:
-        st.warning("Preencha todos os campos antes de priorizar!")
+    if st.button("Priorizar"):
+        if info_identificacao and id_peca:
+            agora = datetime.now(fuso_horario)
+            novo_dado = {
+                "Identificação": info_identificacao,
+                "Peça": id_peca,
+                "Prioridade": info_prio,
+                "Data": agora.strftime("%Y-%m-%d"),
+                "Hora": agora.strftime("%H:%M:%S")
+            }
+            
+            st.session_state["dados"].append(novo_dado)
+            st.session_state["mostrar_botao"] = True
+            st.success("Peça priorizada com sucesso!")
+        else:
+            st.warning("Preencha todos os campos antes de priorizar!")
 
 if st.session_state["dados"]:
     st.subheader("Prioridades Concluídas")
@@ -63,3 +79,5 @@ with col2:
 col1, col2, col3 = st.columns(3)
 with col2:
     st.link_button("Dashboard", "https://lookerstudio.google.com/")
+
+st.markdown("</div>", unsafe_allow_html=True)
